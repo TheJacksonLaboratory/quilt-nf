65b57435bcd5c867ed3aa031bb1fa0383c60eac2
sam-widmayer <samuel.widmayer@jax.org>
2023-01-05 09:49:58 -0500

    make official run script for stitch

---
+++ run_scripts/stitch_mouse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --mail-user=samuel.widmayer@jax.org
+#SBATCH --job-name=STITCH-NF
+#SBATCH --mail-type=END,FAIL
+#SBATCH -p compute
+#SBATCH -q batch
+#SBATCH -t 72:00:00
+#SBATCH --mem=1G
+#SBATCH --ntasks=1
+
+cd $SLURM_SUBMIT_DIR
+
+# LOAD NEXTFLOW
+module use --append /projects/omics_share/meta/modules
+module load nextflow
+
+# RUN PIPELINE
+nextflow ../main.nf \
+--workflow stitch \
+-profile sumner \
+--sample_folder '/fastscratch/widmas/CC_fastqs' \
+--gen_org mouse \
+--pubdir '/fastscratch/STITCH_outputDir' \
+-w '/fastscratch/STITCH_outputDir/work'
+--comment "This script will run haplotype inference using STITCH on mouse samples using default mm10"
