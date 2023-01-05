c87ed90c5ebaec0b8062c3253d9450c6c6fac711
sam-widmayer <samuel.widmayer@jax.org>
2023-01-05 09:31:44 -0500

    add mem and time to run script

--- run_stitch.sh
+++ run_stitch.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=stitch-nf-test
 #SBATCH -p compute
 #SBATCH -q batch
-#SBATCH -t 01:00:00
-#SBATCH --mem=2G
+#SBATCH -t 08:00:00
+#SBATCH --mem=20G
 #SBATCH --ntasks=1
 
 cd $SLURM_SUBMIT_DIR
@@ -18,6 +18,6 @@ nextflow main.nf \
 -profile sumner \
 --workflow stitch \
 --gen_org mouse \
---sample_folder 'test/wgs/mouse' \
+--sample_folder '/fastscratch/widmas/CC_fastqs' \
 --pubdir '/fastscratch/STITCH_outputDir' \
 -w '/fastscratch/STITCH_outputDir/work'
