8471024d07af95b7d405ae6839df140a0c6e042f
sam-widmayer <samuel.widmayer@jax.org>
2023-01-18 12:17:43 -0500

    subsample any set of PE CC reads

--- subsample_CC_reads.sh
+++ subsample_CC_reads.sh
@@ -10,36 +10,19 @@
 
 fastqDir=/fastscratch/widmas/CC_fastqs
 homeDir=/projects/compsci/vmp/USERS/widmas/stitch-nf
-CC007=GES15-06702-CC007-Unc_GAGTGGAT_HMGWYCCXX_L007_001
-CC062=GES15-06751-CC062-Unc_ATGTCAGA_HMHGCCCXX_L008_001
 
 # Fire up the singularity container hopefully
 singularity pull docker://quay.io/biocontainers/seqtk:1.3--hed695b0_2
 
-singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -a ${fastqDir}/${CC007}_R1.fastq.gz > ${fastqDir}/${CC007}_R1.fa
-singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -a ${fastqDir}/${CC007}_R2.fastq.gz > ${fastqDir}/${CC007}_R2.fa
-singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -a ${fastqDir}/${CC062}_R1.fastq.gz > ${fastqDir}/${CC062}_R1.fa
-singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -a ${fastqDir}/${CC062}_R2.fastq.gz > ${fastqDir}/${CC062}_R2.fa
-
-# loop through 20 times and sample 50k reads
-for i in {1..20}
+files=${fastqDir}/GES15-067*
+for f in ${fastqDir}/${files}
 do
-    # subsample CC007 reads
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk sample -s20 ${fastqDir}/${CC007}_R1.fa 50000 > ${homeDir}/test/wgs/mouse/CC007-Unc_50k_${i}_R1.fa
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -F '#' ${homeDir}/test/wgs/mouse/CC007-Unc_50k_${i}_R1.fa > ${homeDir}/test/wgs/mouse/${CC007}_subs${i}_R1.fastq 
-    gzip ${homeDir}/test/wgs/mouse/${CC007}_subs${i}_R1.fastq
+    # find the sample name
+    sample=echo $f |  cut -f 1 -d "."
 
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk sample -s20 ${fastqDir}/${CC007}_R2.fa 50000 > ${homeDir}/test/wgs/mouse/CC007-Unc_50k_${i}_R2.fa
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -F '#' ${homeDir}/test/wgs/mouse/CC007-Unc_50k_${i}_R2.fa > ${homeDir}/test/wgs/mouse/${CC007}_subs${i}_R2.fastq 
-    gzip ${homeDir}/test/wgs/mouse/${CC007}_subs${i}_R2.fastq
-
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk sample -s20 ${fastqDir}/${CC062}_R1.fa 50000 > ${homeDir}/test/wgs/mouse/CC062-Unc_50k_${i}_R1.fa
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -F '#' ${homeDir}/test/wgs/mouse/CC062-Unc_50k_${i}_R1.fa > ${homeDir}/test/wgs/mouse/${CC062}_subs${i}_R1.fastq 
-    gzip ${homeDir}/test/wgs/mouse/${CC062}_subs${i}_R1.fastq
-
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk sample -s20 ${fastqDir}/${CC062}_R2.fa 50000 > ${homeDir}/test/wgs/mouse/CC062-Unc_50k_${i}_R2.fa
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -F '#' ${homeDir}/test/wgs/mouse/CC062-Unc_50k_${i}_R2.fa > ${homeDir}/test/wgs/mouse/${CC062}_subs${i}_R2.fastq 
-    gzip ${homeDir}/test/wgs/mouse/${CC062}_subs${i}_R2.fastq
+    # subsample CC reads
+    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -a ${fastqDir}/$f > ${fastqDir}/${sample}.fa
+    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk sample -s20 ${fastqDir}/${sample}.fa 2000000 > ${homeDir}/test/wgs/mouse/${sample}_${i}.fa
+    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -F '#' ${homeDir}/test/wgs/mouse/${sample}_${i}.fa > ${homeDir}/test/wgs/mouse/${sample}_${i}.fastq
+    gzip ${homeDir}/test/wgs/mouse/${sample}_${i}.fastq
 done
-
-
