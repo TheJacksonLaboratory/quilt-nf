5e0cfd272a7eb53ee7bfcf672f36bc8273437f41
sam-widmayer <samuel.widmayer@jax.org>
2023-01-18 13:52:03 -0500

    fix command substitution for sample names

--- subsample_CC_reads.sh
+++ subsample_CC_reads.sh
@@ -15,14 +15,18 @@ homeDir=/projects/compsci/vmp/USERS/widmas/stitch-nf
 singularity pull docker://quay.io/biocontainers/seqtk:1.3--hed695b0_2
 
 files=${fastqDir}/GES15-067*
-for f in ${fastqDir}/${files}
+for f in ${files}
 do
     # find the sample name
-    sample=echo $f |  cut -f 1 -d "."
+    # echo $f |  cut -f 1 -d "." | cut -f 5 -d "/"
+    sample=$(echo ${f} | cut -f 1 -d "." | cut -f 5 -d "/")
 
     # subsample CC reads
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -a ${fastqDir}/$f > ${fastqDir}/${sample}.fa
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk sample -s20 ${fastqDir}/${sample}.fa 2000000 > ${homeDir}/test/wgs/mouse/${sample}_${i}.fa
-    singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -F '#' ${homeDir}/test/wgs/mouse/${sample}_${i}.fa > ${homeDir}/test/wgs/mouse/${sample}_${i}.fastq
-    gzip ${homeDir}/test/wgs/mouse/${sample}_${i}.fastq
+    for i in {1..20}
+        do
+        singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -a ${f} > ${fastqDir}/${sample}.fa
+        singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk sample -s20 ${fastqDir}/${sample}.fa 2000000 > ${homeDir}/test/wgs/mouse/${sample}_${i}.fa
+        singularity exec ${homeDir}/seqtk_1.3--hed695b0_2.sif seqtk seq -F '#' ${homeDir}/test/wgs/mouse/${sample}_${i}.fa > ${homeDir}/test/wgs/mouse/${sample}_${i}.fastq
+        gzip ${homeDir}/test/wgs/mouse/${sample}_${i}.fastq
+        done
 done
