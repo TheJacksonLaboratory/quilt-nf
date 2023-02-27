c0cbf3b7f51acba69ad684e45b4b555bb3683081
sam-widmayer <samuel.widmayer@jax.org>
2023-02-27 11:52:24 -0500

    run singularity container once

--- bin/stitch/create_founder_ref_file.sh
+++ bin/stitch/create_founder_ref_file.sh
@@ -5,8 +5,9 @@
 #SBATCH --mem=100G
 #SBATCH --ntasks=1
 
+singularity run /projects/compsci/omics_share/meta/containers/quay.io-biocontainers-bcftools-1.15--h0ea216a_2.img
+
 # create a B6 vcf by changing B6/NJ alt alleles to ref
-singularity exec /projects/compsci/omics_share/meta/containers/quay.io-biocontainers-bcftools-1.15--h0ea216a_2.img \
 bcftools view /projects/omics_share/mouse/GRCm39/genome/annotation/snps_indels/rel_2112_v8/mgp_REL2021_snps.vcf.gz \
 -m2 -M2 -v snps \
 -s C57BL_6NJ | \
@@ -15,12 +16,11 @@ sed 's/0\/1/0\/0/g' | \
 sed 's/1\/0/0\/0/g' | \
 sed 's/C57BL_6NJ/B/g' | \
 bcftools view -Oz -o /fastscratch/widmas/B6.vcf.gz
+
 # index B6 vcf
-singularity exec /projects/compsci/omics_share/meta/containers/quay.io-biocontainers-bcftools-1.15--h0ea216a_2.img \
 tabix -p vcf /fastscratch/widmas/B6.vcf.gz
 
 # create the other main vcf
-singularity exec /projects/compsci/omics_share/meta/containers/quay.io-biocontainers-bcftools-1.15--h0ea216a_2.img \
 bcftools view /projects/omics_share/mouse/GRCm39/genome/annotation/snps_indels/rel_2112_v8/mgp_REL2021_snps.vcf.gz \
 -m2 -M2 -v snps \
 -s A_J,C57BL_6NJ,129S1_SvImJ,NOD_ShiLtJ,NZO_HlLtJ,CAST_EiJ,PWK_PhJ,WSB_EiJ | \
@@ -33,11 +33,10 @@ sed 's/CAST_EiJ/F/g' | \
 sed 's/PWK_PhJ/G/g' | \
 sed 's/WSB_EiJ/H/g' | \
 bcftools view -Oz -o /fastscratch/widmas/notB6.vcf.gz
+
 # index main vcf
-singularity exec /projects/compsci/omics_share/meta/containers/quay.io-biocontainers-bcftools-1.15--h0ea216a_2.img \
 tabix -p vcf /fastscratch/widmas/notB6.vcf.gz
 
 # join the two, but select the altered B6 sample field
-singularity exec /projects/compsci/omics_share/meta/containers/quay.io-biocontainers-bcftools-1.15--h0ea216a_2.img \
 bcftools merge /fastscratch/widmas/B6.vcf.gz /fastscratch/widmas/notB6.vcf.gz --force-samples | \
 bcftools view -s A,B,C,D,E,F,G,H -Oz -o bin/stitch/DO_founders.vcf.gz
