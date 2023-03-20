156cf70658185888246f1043580edc2fe4e4aaaa
sam-widmayer <samuel.widmayer@jax.org>
2023-03-20 14:54:07 -0400

    fix read designation -I

--- fastp_test.sh
+++ fastp_test.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 #SBATCH --job-name=fastp_test
@@ -10,15 +9,10 @@
 #SBATCH --mem=100G
 #SBATCH --ntasks=1
 
-singularity exec docker://sjwidmay/fastp_nf:fastp /fastp -i /fastscratch/STITCH_outputDir/work/e7/1ffbfb7263b926b9a0c400f77ddab6/plexWell-M24_GT23-00380_CTGGTCGT-ATCTATTT_S36_L001_R1_001.fastq.gz_filtered_trimmed -I /fastscratch/STITCH_outputDir/work/e7/1ffbfb7263b926b9a0c400f77ddab6/plexWell-M24_GT23-00380_CTGGTCGT-ATCTATTT_S36_L001_R2_001.fastq.gz_filtered_trimmed -o test.R1.fq -O test.R2.fq
+read1=/fastscratch/STITCH_outputDir/work/e7/1ffbfb7263b926b9a0c400f77ddab6/plexWell-M24_GT23-00380_CTGGTCGT-ATCTATTT_S36_L001_R1_001.fastq.gz_filtered_trimmed
+read2=/fastscratch/STITCH_outputDir/work/e7/1ffbfb7263b926b9a0c400f77ddab6/plexWell-M24_GT23-00380_CTGGTCGT-ATCTATTT_S36_L001_R2_001.fastq.gz_filtered_trimmed
+outDir=/projects/compsci/vmp/USERS/widmas/stitch-nf/data/DO_seqwell_NovaSeq/fastp_test
+singularity exec docker://sjwidmay/fastp_nf:fastp /fastp -i ${read1} -I ${read2} -o ${outDir}/test.R1.fq -O ${outDir}/test.R2.fq --detect_adapter_for_pe -g -D -c -p
 
-# detect adapters automatically
-#        --detect_adapter_for_pe \
-# detect and remove duplicates?
-#       -D \
 # polyG trimming
-#        -g \
-# base correction analysis
-#        -c \
-# overrepresentation analysis
-#        -p   
+#        -g \   
