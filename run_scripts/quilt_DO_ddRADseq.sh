03c3aafdd922affd3528e1095b3f21938123012c
sam-widmayer <samuel.widmayer@jax.org>
2024-06-19 09:41:15 -0400

    test push

--- run_scripts/quilt_DO_ddRADseq.sh
+++ run_scripts/quilt_DO_ddRADseq.sh
@@ -4,20 +4,20 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH -p compute
 #SBATCH -q batch
-#SBATCH -t 36:00:00
-#SBATCH --mem=1G
+#SBATCH -t 72:00:00
+#SBATCH --mem=50G
 #SBATCH --ntasks=1
 
 cd $SLURM_SUBMIT_DIR
 
 # LOAD NEXTFLOW
 module use --append /projects/omics_share/meta/modules
-module load nextflow
+module load nextflow/23.10.1
 
 # RUN PIPELINE
 nextflow main.nf \
 --workflow quilt \
--profile sumner \
+-profile sumner2 \
 --sample_folder '/projects/compsci/vmp/lcgbs_ssif/data/novaseq_ddradseq2/stacks_fastqs/raw/DO' \
 --gen_org mouse \
 --pubdir '/projects/compsci/vmp/lcgbs_ssif/results/quilt' \
@@ -25,7 +25,8 @@ nextflow main.nf \
 --pattern "*.{1,2}.*" \
 --library_type "ddRADseq" \
 --run_name $1 \
--w '/fastscratch/STITCH_outputDir/work' \
+-w '/flashscratch/STITCH_outputDir/work' \
+--bin_shuffling_file '/projects/compsci/vmp/USERS/widmas/quilt-nf/data/shuffle_bins.csv' \
 --downsample_to_cov '/projects/compsci/vmp/USERS/widmas/quilt-nf/data/downsampling_values.csv' \
 --cross_type 'do' \
 --ref_file_dir '/projects/compsci/vmp/lcgbs_ssif/data/DO_founders' \
